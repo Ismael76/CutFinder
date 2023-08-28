e8f12331bf251a457af7a81aa577acb23872b8c3
rakib2067 <rakib7@hotmail.co.uk>
2023-08-28 20:48:32 +0100

    Fixed Migration and seed file

--- src/config/dev_seeds.sql
+++ src/config/dev_seeds.sql
@@ -6,19 +6,15 @@ VALUES
 ('Imran','im@im.cob', 'pass'),
 ('Shah','s@s.cob', 'pass');
 
-
-INSERT INTO barbers (user_id)
+INSERT INTO barbershops (shopname, address, storenumber)
 VALUES
-(1),
-(2),
-(3);
+('Smokeys Barbers', '128 Windsor Road', '02082329323');
 
-INSERT INTO barbershops (shopname, owner_id, address, storenumber)
+INSERT INTO barbers (user_id, barbershop_id)
 VALUES
-('Smokeys Barbers', 1, '128 Windsor Road', '02082329323');
-
-UPDATE barbers
-SET barbershop_id = 1;
+(1, 1),
+(2, 1),
+(3, 1);
 
 INSERT INTO hairstyles (name)
 VALUES
@@ -34,7 +30,7 @@ VALUES
 (3,1, 20.00),
 (4,1, 20.00);
 
-INSERT INTO barberSkills (hairstyle, barber_id)
+INSERT INTO barberSkills (hairstyle_id, barber_id)
 VALUES
 (1, 1),
 (2, 1),
@@ -43,11 +39,11 @@ VALUES
 (1, 2);
 
 
-INSERT INTO bookings (booking_day, customer_id, barber_id, hairstyle)
+INSERT INTO bookings (booking_day, customer_id, barber_id, hairstyle_id)
 VALUES
 ('Monday 11th August',1, 1, 1),
 ('Monday 13th August',2, 1, 2),
 ('Wednesday 17th September',2, 2, 3),
 ('Tuesday 29th October',3, 3, 1),
 ('Saturday 31st July',4, 2, 1),
-('Monday 18th January',3, 3);
+('Monday 18th January',3, 3, 1);
